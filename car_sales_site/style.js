24233d60f29f2f4b829baf28a5b8639cb88fa3d4
Joan <joan.mascarell205@comunidadunir.net>
2025-10-27 10:46:36 +0100

    Actualización

---
+++ car_sales_site/style.js
@@ -0,0 +1,27 @@
+const menuBtn = document.querySelector('.menu')
+panel = document.querySelector('.filter')
+panelStyles = window.getComputedStyle(panel)
+hearts = document.querySelectorAll('.fa-heart')
+
+menuBtn.addEventListener('click', () => {
+
+    if (panelStyles.getPropertyValue('clip-path') === 'circle(0% at 100% 0%)') {
+        panel.style.clipPath = 'circle(100% at 50% 50%)'
+    }
+
+    else {
+        panel.style.clipPath = 'circle(0% at 100% 0%)'
+    }
+})
+
+document.addEventListener('mouseup', (event) => {
+    if (panelStyles.getPropertyValue('clip-path') === 'circle(100% at 50% 50%)') {
+        if(!panel.contains(event.target))
+            panel.style.clipPath = 'circle(0% at 100% 0%)'
+    }
+})
+
+function changeHeart(x) {
+    x.classList.toggle("fa-regular");
+    x.classList.toggle("fa-solid");
+}
